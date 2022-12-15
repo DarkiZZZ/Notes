72329e1dbdced7420ea6daea34cdb44c1ee619bb
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-15 23:43:52 +0300

    added binding for testing

--- app/src/main/java/ru/msokolov/notesapp/screens/note/show/NoteFragment.kt
+++ app/src/main/java/ru/msokolov/notesapp/screens/note/show/NoteFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.msokolov.notesapp.R
+import ru.msokolov.notesapp.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
 
@@ -15,18 +16,22 @@ class NoteFragment : Fragment() {
     }
 
     private lateinit var viewModel: NoteViewModel
+    private lateinit var binding: FragmentNoteBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+        binding = FragmentNoteBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
         // TODO: Use the ViewModel
+
+
     }
 
 }
